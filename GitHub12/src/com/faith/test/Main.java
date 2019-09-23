3cf8da3dae9ae83fd4e5730c7f5f9138c1efeee8
HP <HP@HOONIGAN>
2019-09-23 15:14:55 +0530

    initial commit

---
+++ GitHub12/src/com/faith/test/Main.java
@@ -0,0 +1,15 @@
+package com.faith.test;
+
+public class Main {
+	public static void main(String[] args){
+		SalesData objSalesData=new SalesData();
+		System.out.println("Hello Ust guys");
+		displayGreetings();
+		objSalesData.display();
+		
+	}
+	private static void displayGreetings(){
+		System.out.println("Welcome to sales Report App 2019");
+	}
+
+}
